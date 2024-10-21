3736924b46509e5bb4a7d25540db33381adf758a
ahmedullahmern <ahmedullahmern@gmail.com>
2024-10-22 01:11:00 +0500

    practice antds

--- src/confige/routeing.jsx
+++ src/confige/routeing.jsx
@@ -1,12 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/home/home'
+import AppCheck from '../pages/check/check'
+import AppLayout from '../pages/layout/layout'
 
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<HomePage />} />
+
+                <Route
+                    path={"/"} element={
+                        <AppLayout>
+                            <HomePage />
+                        </AppLayout>
+                    }
+
+                />
+
+                <Route path={"/check"} element={
+                    <AppLayout>
+                        <AppCheck />
+                    </AppLayout>
+                } />
             </Routes>
         </BrowserRouter>
     )
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,34 +1,48 @@
 import '../css/style.scss'
 import LOGO from '../../assets/logo.svg'
+import { Link, useNavigate } from 'react-router-dom'
+import { Spin } from 'antd'
+import { useState } from 'react'
 
 function HomePage() {
+    let [isLlader, isuseLoader] = useState(false)
+
+
+    // isuseLoader(true)
     return (
-        <div className="container">
-            <div className="header-bar">
-                <div className="imges">
-                    <img src={LOGO} alt="" />
+        <>
+            {isLlader ? <Spin size="large" />
+                :
+                <div className="container">
+                    <div className="header-bar">
+                        <div className="imges">
+                            <img src={LOGO} alt="" />
+                        </div>
+                        <div className='list-header'>
+                            <ul>
+                                <li>
+                                    How it works
+                                </li>
+                                <li>
+                                    Download
+                                </li>
+                                na
+                                <li>
+                                    Upgrade
+                                </li>
+
+                                <li>
+                                    Feedback
+                                </li>
+                                <li>
+                                    Login / Register
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className='list-header'>
-                    <ul>
-                        <li>
-                            How it works
-                        </li>
-                        <li>
-                            Download
-                        </li>
-                        <li>
-                            Upgrade
-                        </li>
-                        <li>
-                            Feedback
-                        </li>
-                        <li>
-                            Login / Register
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 export default HomePage
